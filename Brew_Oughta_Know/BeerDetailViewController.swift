4c9d0281a5839f5571f2e632d58092731764a78b
Patrick Landin <patricklandin@gmail.com>
2015-03-10 13:17:52 -0700

    Created BeerDetailVC.

---
+++ Brew_Oughta_Know/BeerDetailViewController.swift
@@ -0,0 +1,23 @@
+//
+//  BeerDetailViewController.swift
+//  Brew_Oughta_Know
+//
+//  Created by Patrick Landin on 3/10/15.
+//  Copyright (c) 2015 pLandin. All rights reserved.
+//
+
+import UIKit
+
+class BeerDetailViewController: UIViewController {
+
+  @IBOutlet weak var breweryLabel: UILabel!
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+      self.breweryLabel.text = 
+
+        // Do any additional setup after loading the view.
+    }
+
+}
